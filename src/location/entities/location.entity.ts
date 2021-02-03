0c433f28bccefee1f2d803d9a82f6fbec9f54306
Rajeshwari-Rudra <rajeshwarihyd.02@gmail.com>
2021-02-03 08:25:19 -0600

    add entities

--- src/location/entities/location.entity.ts
+++ src/location/entities/location.entity.ts
@@ -1,1 +1,16 @@
-export class Location {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Location {
+    @PrimaryGeneratedColumn()
+id: number;
+
+    @Column()
+    latitude: number;
+
+    @Column()
+    longitude: number;
+
+    @Column({ default: true })
+    isActive: boolean;
+}
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,19 @@
-export class User {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+     @Column()
+    email: string;
+
+    // @Column()
+    // firstName: string;
+
+    // @Column()
+    // lastName: string;
+
+    @Column({ default: true })
+    isActive: boolean;
+}
