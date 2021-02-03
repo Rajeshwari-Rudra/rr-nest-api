9be90032a18e65ba3b9f382bc23933e785b34368
Rajeshwari-Rudra <rajeshwarihyd.02@gmail.com>
2021-02-03 06:21:06 -0600

    add resource location

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QuestModule } from './quest/quest.module';
 import { UserModule } from './user/user.module';
+import { LocationModule } from './location/location.module';
 
 @Module({
-  imports: [QuestModule, UserModule],
+  imports: [QuestModule, UserModule, LocationModule],
   controllers: [AppController],
   providers: [AppService],
 })
