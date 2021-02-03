34c849cfae08fa9640f9351027268c2ff63c7927
Rajeshwari-Rudra <rajeshwarihyd.02@gmail.com>
2021-02-03 07:53:54 -0600

    database integration library typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,22 @@ import { AppService } from './app.service';
 import { QuestModule } from './quest/quest.module';
 import { UserModule } from './user/user.module';
 import { LocationModule } from './location/location.module';
+import { TypeOrmModule } from '@nestjs/typeorm'; 
+import { User } from './user/entities/user.entity';
+import { Location } from './location/entities/location.entity';
+
 
 @Module({
-  imports: [QuestModule, UserModule, LocationModule],
+  imports: [TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 3000,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [User,Location],
+      synchronize: true,
+    }),QuestModule, UserModule, LocationModule,],
   controllers: [AppController],
   providers: [AppService],
 })
