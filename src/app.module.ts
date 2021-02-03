56b5d7694c153cd507f05567345cf7f30e47c46f
Rajeshwari-Rudra <rajeshwarihyd.02@gmail.com>
2021-02-03 06:10:53 -0600

    add resource user

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QuestModule } from './quest/quest.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [QuestModule],
+  imports: [QuestModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
