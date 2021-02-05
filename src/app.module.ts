c8195ea2f5162684642c3a60d6c8b69c65dd84d9
Rajeshwari-Rudra <rajeshwarihyd.02@gmail.com>
2021-02-05 12:37:06 -0600

    connect database

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,16 +5,13 @@ import { QuestModule } from './quest/quest.module';
 import { UserModule } from './user/user.module';
 import { LocationModule } from './location/location.module';
 import { TypeOrmModule } from '@nestjs/typeorm'; 
-import { User } from './user/entities/user.entity';
-import { Location } from './location/entities/location.entity';
 import { Connection } from 'typeorm';
-import { Quest } from './quest/entities/quest.entity';
+
 
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(
-     ),QuestModule, UserModule, LocationModule,],
+    TypeOrmModule.forRoot(),QuestModule, UserModule, LocationModule,],
   controllers: [AppController],
   providers: [AppService],
 })
